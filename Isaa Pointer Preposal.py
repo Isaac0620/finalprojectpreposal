a4369dc714fc07aedb75d85729aa2ce26a8fcf1c
Isaac0620 <75231157+Isaac0620@users.noreply.github.com>
2020-11-29 21:52:18 -0500

    Add files via upload

---
+++ Isaa Pointer Preposal.py
@@ -0,0 +1,16 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>>  Preproposal
+
+## What idea(s) do you have for your final project?
+
+A pong Game but very very difficult
+
+## If you plan to collaborate with one or two classmates, what are their names?
+
+Zamir shelton
+Jude piacentino
+
+## Do you have any questions of your own?
+
+TODO
